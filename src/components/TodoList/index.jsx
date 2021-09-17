ec3fac1c8fd1d94be2f63f7c2ee823f2895630ae
Kakashi vp <ngoduong37na@gmail.com>
2021-09-17 14:37:37 +0700

    update code

---
+++ src/components/TodoList/index.jsx
@@ -0,0 +1,59 @@
+import React,{useState} from 'react'
+import TodoForm from './../TodoForm/index';
+import Todo from './../Todo/index';
+import '../styles.scss';
+function TodoList() {
+
+    const [todos, setTodos] = useState([]);
+
+    const addTodo = todo =>{
+        if(!todo.text || /^\s*$/.test(todo.text)){
+            return 
+        }
+
+        const newTodos = [todo,...todos];
+        
+        setTodos(newTodos);
+        console.log(...todos);
+    }
+
+    const completeTodo = id =>{
+        let updatedTodos =todos.map(todo =>{
+            if(todo){
+                todo.isComplete =!todo.isComplete;
+            }
+        return todo
+        })
+        setTodos(updatedTodos);
+        
+    }
+
+    const updateTodo =(todoID,newValue) => {
+      
+            if(!newValue.text || /^\s*$/.test(newValue.text)){
+                return 
+            }
+
+            setTodos(prev =>prev.map(item => (item.id ===todoID?newValue :item)))
+}
+
+    const removeTodo = id =>{
+        const removeArr=[...todos].filter(todo =>todo.id !==id)
+
+        setTodos(removeArr);
+    }
+
+    return (
+        <div className="todo-app">
+            <h1> What's the Plan for today</h1>
+            <TodoForm onSubmit={addTodo} />
+            <Todo todos={todos} 
+            completeTodo={completeTodo} 
+            removeTodo={removeTodo}
+            updateTodo={updateTodo}
+            />
+        </div>
+    )
+}
+    export default TodoList
+    
